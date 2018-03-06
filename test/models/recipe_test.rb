84f0b9ffd8dad666892696a4130228eb09ee019b
sandervrielink <sander.vrielink@gmail.com>
2018-03-06 22:42:27 +0100

    add one to many to recipes and chefs

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,11 +3,17 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "vegetable", description: "great vegetable recipe")
+    @chef = Chef.create!(chefname: "Sander", email: "sander@sander.com")
+    @recipe = @chef.recipes.build(name: "vegetable", description: "great vegetable recipe")
   end
 
   test "recipe should be valid" do
     assert @recipe.valid?
+  end
+
+  test "recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "name should be present" do
